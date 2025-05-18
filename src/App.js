cc2fb945349d31c6d402a59125cf5811ddef735e
M <m@Mui-MacBookPro.local>
2025-05-18 22:29:09 +0900

    리액트 라우터 설정 및 파일 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import GroupPurchase from "./pages/GroupPurchase";
+import GroupPurchaseDetail from "./pages/GroupPurchaseDetail";
+import Rental from "./pages/Rental";
+import RentalDetail from "./pages/RentalDetail";
+import Community from "./pages/Community";
+import Reviews from "./pages/Reviews";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/group-purchase" element={<GroupPurchase />} />
+        <Route path="/group-purchase/:id" element={<GroupPurchaseDetail />} />
+        <Route path="/rental" element={<Rental />} />
+        <Route path="/rental/:id" element={<RentalDetail />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,4 @@
+function Home() {
+  return <h2>🏠 Home 페이지입니다</h2>;
+}
+export default Home;
