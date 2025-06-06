1b2bd0d0d1c7620c13b6ba4d3ba0035ff5e60898
sodaeng0051 <sjung0051@naver.com>
2025-06-06 20:26:57 +0900

    수정

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,21 @@
 import './Search.css';
-import React from "react";
-import SearchIcon from './Search - 0.svg'
+// import SearchIcon from './Vector-0.svg';
 
 const Search = () => {
-  	return (
-    		<div className="search">
-      			<div className="depth-4-frame-0">
-        				<img className="depth-5-frame-0" alt="" src={SearchIcon} />
-        				<div className="depth-5-frame-1">
-          					<div className="search1">Search</div>
-        				</div>
-      			</div>
-    		</div>);
+  return (
+    <div className="search-container">
+      <img
+        src="/Vector-0.svg"
+        alt="Search Icon"
+        className="search-icon"
+      />
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-input"
+      />
+    </div>
+  );
 };
 
 export default Search;
